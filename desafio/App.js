0c4ab0d39d7e45103850d9276bbce4b474c687b2
mariadelaverde98 <mariadelaverde98@gmail.com>
2023-01-13 12:40:21 +0100

    hey

--- desafio/App.js
+++ desafio/App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>eyyyyyyyyyyyyyyy!!!!!!</Text>
+      <Text style={styles.texto}> </Text>
+      <Text style={styles.henalu}>
+        hola viti wapo!!! eyyyy
+      </Text>
+      <TextInput placeholder='Escribe algo...' ></TextInput>
       <StatusBar style="auto" />
+      
     </View>
   );
 }
@@ -13,8 +18,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
+    textDecorationColor: 'white',
+    color: 'white', 
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  texto: {
+    color: "white",
+    fontSize: "100px"
+  },
+
+  henalu: {
+    color: 'blue',
+    fontSize: '100px'
+  }
+
 });
---
+++ desafio/Login.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+export default function Login() {
+  return (
+    <View>
+      Hola
+    </View>
+  );
+}
+
+
